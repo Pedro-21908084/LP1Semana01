7b0d5b13e518ee7259be2df2cdb1cc48773b2cfa
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-23 18:52:40 +0000

    Finished exercise 1

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //cycles every argument given in args
+            for (int i = 0; i<args.Length;i++)
+            {
+                //prints the argument in args[i]
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
+
