62397aa6ba7546bf6cc352773ff49c658657c265
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-23 19:26:02 +0000

    Exercise3 ints

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration and initialization of ints
+            int inteiro1 = -12;
+            uint inteiro2 = 20;
+            
+            //printing of the variables
+            Console.WriteLine(string.Format("Inteiro: {0} \nInteiro: {1}", inteiro1, inteiro2));
+        }
+    }
+}
