7527ab71082332eeef0e3b567d0fda6509cf4dd2
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-23 19:10:27 +0000

    Finished exercise 2

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -4,10 +4,10 @@ namespace Argumentos
 {
     class Program
     {
-        static void Main(string [] args)
+        static void Main(string[] args)
         {
             //cycles every argument given in args
-            for (int i = 0; i<args.Length;i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 //prints the argument in args[i]
                 Console.WriteLine(args[i]);
---
+++ OlaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        /// <summary>
+        /// It runs automatically when the program starts, printing the 2 strings, hello and bye world
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Bye world!");
+        }
+    }
+}
