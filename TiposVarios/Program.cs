f81eea6c446621af6809b83d535b00c5a15cb3e7
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-23 20:44:45 +0000

    Exercise3 Characters

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Argumentos
 {
@@ -6,12 +7,20 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             //declaration and initialization of ints
             int inteiro1 = -12;
-            uint inteiro2 = 20;
-            
+            uint inteiro2 = 20U;
+
+            //declaration and initialization of chars
+            char character1 = '\u1f9a';
+            char character2 = '\u1f60';
+            char character3 = '\u03c0';
+
+
             //printing of the variables
-            Console.WriteLine(string.Format("Inteiro: {0} \nInteiro: {1}", inteiro1, inteiro2));
+            Console.WriteLine(string.Format("Inteiro: {0} \nInteiro sem sinal: {1}\nCharacter: {2}\nCharacter: {3}\nCharacter: {4}", inteiro1, inteiro2, character1, character2, character3));
         }
     }
 }
