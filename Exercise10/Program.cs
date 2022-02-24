6465cacce3788575feec46f9863e273a48511cf7
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-24 11:28:10 +0000

    Exercise 10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString;
+            
+            Console.WriteLine("Hello, Please give me a string:");
+            userString =  Console.ReadLine();
+            Console.WriteLine("Did you write:");
+            Console.WriteLine(userString + " ?");
+        }
+    }
+}
