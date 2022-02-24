34bc688295023c3072a9e87a5883bd5c8e1e4422
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-24 12:23:33 +0000

    Exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration of variables
+            string userNumber1;
+            string userNumber2;
+            float number1;
+            float number2;
+
+            //Read user input to variables
+            Console.Write("Hello, please tell me a real number(seperated by ','): ");
+            userNumber1 = Console.ReadLine();
+
+            Console.Write("Now, tell me one more: ");
+            userNumber2 = Console.ReadLine();
+
+            //Convert string to float
+            number1 = (float)Convert.ToDouble(userNumber1);
+            number2 = (float)Convert.ToDouble(userNumber2);
+
+            //Print results
+            Console.WriteLine($"{number1} to the power of {number2} is: {Math.Pow(number1, number2):f4}");
+        }
+    }
+}
