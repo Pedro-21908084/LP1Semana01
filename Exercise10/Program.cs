c0fa59385f485ad12790b8f5c5772f38ef6e1ac1
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-24 11:44:31 +0000

    Exercise 11 and reformating of exercise 10

--- Exercise10/Program.cs
+++ Exercise10/Program.cs
@@ -7,9 +7,9 @@ namespace Exercise10
         static void Main(string[] args)
         {
             string userString;
-            
+
             Console.WriteLine("Hello, Please give me a string:");
-            userString =  Console.ReadLine();
+            userString = Console.ReadLine();
             Console.WriteLine("Did you write:");
             Console.WriteLine(userString + " ?");
         }
---
+++ Exercise11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of varianles needed
+            string userString;
+
+            //Get user string
+            Console.WriteLine("Hello, Please give me a string:");
+            userString = Console.ReadLine();
+
+            //invert string in print
+            for (int i = 0; i < userString.Length; i++)
+            {
+                Console.Write(userString[userString.Length - i - 1]);
+            }
+            //print end of line
+            Console.Write("\n");
+        }
+    }
+}
