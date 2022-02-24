881dba20e1f0b8f84bd9185f0ed4d6af277830a6
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-24 12:51:58 +0000

    Correction of exercise 16

--- Exercise16/Program.cs
+++ Exercise16/Program.cs
@@ -9,10 +9,10 @@ namespace Exercise16
             //Declaration of variables
             string userNumber1;
             string userNumber2;
-            string userResult;
+            string userNumber3;
             int number1;
             int number2;
-            int result;
+            int number3;
 
             Console.WriteLine("Hello, in this program ill tell you if two numbers given result in the third.");
 
@@ -21,16 +21,24 @@ namespace Exercise16
             userNumber1 = Console.ReadLine();
             Console.Write("Now the second number: ");
             userNumber2 = Console.ReadLine();
-            Console.Write("And finally, tell me the result of their addition: ");
-            userResult = Console.ReadLine();
+            Console.Write("And finally, tell me the last number: ");
+            userNumber3 = Console.ReadLine();
 
             //Convert to int
             number1 = Convert.ToInt32(userNumber1);
             number2 = Convert.ToInt32(userNumber2);
-            result = Convert.ToInt32(userResult);
+            number3 = Convert.ToInt32(userNumber3);
 
             //Check and print results
-            if (number1 + number2 == result)
+            if (number1 + number2 == number3)
+            {
+                Console.WriteLine("Is possible :)");
+            }
+            else if (number2 + number3 == number1)
+            {
+                Console.WriteLine("Is possible :)");
+            }
+            else if (number1 + number3 == number2)
             {
                 Console.WriteLine("Is possible :)");
             }
