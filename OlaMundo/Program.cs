b7e61f60d076e89e6f9bde77b3a0cfcd258ba629
Pedro Fernandes <pm_rf@hotmail.com>
2022-02-24 00:49:00 +0000

    Reformating for 80 charcter line limit

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -5,7 +5,8 @@ namespace Argumentos
     class Program
     {
         /// <summary>
-        /// It runs automatically when the program starts, printing the 2 strings, hello and bye world
+        /// It runs automatically when the program starts, 
+        /// printing the 2 strings, hello and bye world
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
